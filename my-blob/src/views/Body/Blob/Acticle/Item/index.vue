ce1a574236d9912d10d2f6a587d3299aa57ebf48
weiwenyu <2454150875@qq.com>
2022-07-25 20:51:31 +0800

    添加404的配置

--- my-blob/src/views/Body/Blob/Acticle/Item/index.vue
+++ my-blob/src/views/Body/Blob/Acticle/Item/index.vue
@@ -4,7 +4,7 @@
  * @Author: wwy
  * @Date: 2022-07-12 10:58:19
  * @LastEditors: wwy
- * @LastEditTime: 2022-07-13 20:15:43
+ * @LastEditTime: 2022-07-22 18:07:27
 -->
 <template>
   <div class="blob-activle-text-box">
@@ -67,74 +67,31 @@
 </template>
 
 <script>
+import { onMounted } from "vue";
 export default {
   name: "BlobActivleTextView",
-  setup() {
-    const activleArray = [
-      {
-        activleId: 1,
-        activlePhoto: require("@/assets/images/1.jpg"),
-        activleTitle: "Title Goes Hear",
-        activleTime: "2022年7月12日13:54:00",
-        activleTag: "JavaScript",
-        activleBody: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
-        explicabo quis illo possimus aspernatur iusto, praesentium dolores,`,
-        activleUserPhoto: require("@/assets/images/p.jpg"),
-      },
-      {
-        activleId: 2,
-        activlePhoto: require("@/assets/images/2.png"),
-        activleTitle: "JavaScript is a programming language",
-        activleTime: "2022年7月12日13:54:00",
-        activleTag: "JavaScript",
-        activleBody: `JavaScript is a programming language that adds interactivity to your website. This happens in games, in the behavior of responses when buttons are pressed or with data entry on forms; with dynamic styling; with animation, etc. This article helps you get started with JavaScript and furthers your understanding of what is possible`,
-        activleUserPhoto: require("@/assets/images/p.jpg"),
-      },
-      {
-        activleId: 3,
-        activlePhoto: require("@/assets/images/3.jpg"),
-        activleTitle: "日期选择器",
-        activleTime: "2022年7月12日13:54:00",
-        activleTag: "JavaScript",
-        activleBody: `日期选择器作为基础组件重要不可或缺的一员，大家已经快习惯它一成不变的样子，输入框+日期选择弹出层。但到业务中，这种墨守成规的样子真的能百分百契合业务需求吗。这篇文章从多个网站的日期选择场景出发，企图归纳出日期选择器的最佳实践。这篇文章对移动端的日期选择暂无涉猎，都是 PC 端，列举出通用场景，每个类型日期选择器需要考虑的设计。 文章链接：Designing The Perfect Date And Time Picker 感谢本期评论官 @黄子毅 @流形 @王亮 @赵阳 @不知名的花瓣工程师`,
-        activleUserPhoto: require("@/assets/images/p.jpg"),
-      },
-      {
-        activleId: 4,
-        activlePhoto: require("@/assets/images/4.jpg"),
-        activleTitle:
-          "国内目前处于 BI 1.0 阶段，也就是报表阶段，因此笔者将阐述这个阶段 BI 的核心开发概念",
-        activleTime: "2022年7月12日13:54:00",
-        activleTag: "JavaScript",
-        activleBody: `数据集即数据的集合，在 BI 领域更多指一种标准化的数据结构。
-        任何数据都可以封装成数据集，比如 txt 文本、excel、mysql 数据库等等。
-        数据集的基本形态是二维表格，列头表示字段，每一行就是一份数据，数据展示就是通过对这些数据字段进行多维度分析`,
-        activleUserPhoto: require("@/assets/images/p.jpg"),
-      },
-      {
-        activleId: 5,
-        activlePhoto: require("@/assets/images/5.jpg"),
-        activleTitle:
-          "javascript 的 this 是个头痛的话题，本期精读的文章更是引出了一个观点，避免使用 this。我们来看看是否有道理",
-        activleTime: "2022年7月12日13:54:00",
-        activleTag: "JavaScript",
-        activleBody: `初学者可能突然将 this 弄丢导致程序出错，甚至在 react 中也要使用 bind 的方式，使回调可以访问到 setState 等函数。
-        this 也不利于测试，如果使用纯函数，可以通过入参出参做测试，而不需要预先初始化环境。`,
-        activleUserPhoto: require("@/assets/images/p.jpg"),
-      },
-      {
-        activleId: 6,
-        activlePhoto: require("@/assets/images/6.jpg"),
-        activleTitle:
-          "尤雨溪 在 2019 JSConf 的分享 Seeking the Balance in Framework Design 十分精彩，道出了如何进行合理的前端框架设计与框架选型",
-        activleTime: "2022年7月12日13:54:00",
-        activleTag: "JavaScript",
-        activleBody: `服务范围是个比较难翻译的单词，在原 PPT 中用了 “Scope” 这个单词表示，可以理解为 “作用域、框架的承诺功能范围、服务配套齐全程度”。比如提供的是一个工具库还是整体框架，插件管理是集中式还是依赖生态。
-        React 是典型的小服务范围框架，核心包只实现了基本功能，而其他生态基本靠社区拓展；Angular 是典型大服务范围框架，官方对所有业务场景都做了最佳实践能力覆盖；Vue 处在中间区域，通过功能分层，既拥有小服务范围的能力，又可以搭配官方插件实现更多场景化能力`,
-        activleUserPhoto: require("@/assets/images/p.jpg"),
-      },
-    ];
-    return { activleArray };
+
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  setup(prop) {
+    // 为所有图片添加加载异常处理
+    const handleImgLoadError = () => {
+      const imgs = document.querySelectorAll("img");
+      imgs.forEach((item) => {
+        item.addEventListener("error", function () {
+          this.src = require("@/assets/images/404.jpg");
+        });
+      });
+    };
+
+    onMounted(() => handleImgLoadError());
+
+    return { activleArray: prop.data };
   },
 
   methods: {
