7a2d6e420935a5c5e3396092bfc706e8db57814d
weiwenyu <2454150875@qq.com>
2022-08-11 20:37:18 +0800

    图片更改

--- my-blob/public/config.js
+++ my-blob/public/config.js
@@ -10,7 +10,7 @@ window._activleArray = [
     activleBody: `本文主要介绍的是如何docker安装Guacamole,进行浏览器远程桌面的访问
     以及进行springboot + vue 与Guacamole的集成`,
     activleAuthor: "洛琪希",
-    activleUserPhoto: "img/user-images/wwy.jpg",
+    activleUserPhoto: "img/user-images/wwy.webp",
   },
   {
     activleId: "git常用操作",
@@ -22,7 +22,7 @@ window._activleArray = [
     activleTag: "Git",
     activleBody: `这里记录了关于git命令的一些常用操作,简单详细！`,
     activleAuthor: "洛琪希",
-    activleUserPhoto: "img/user-images/wwy.jpg",
+    activleUserPhoto: "img/user-images/wwy.webp",
   },
   {
     activleId: "jsDom部分写法性能优化",
@@ -36,7 +36,7 @@ window._activleArray = [
     activleBody:
       "通过DOM方法`removeChild()`和`replaceChild()`删除节点，`innerHTML`替换节点的情况下,如果被删除替换的节点中有事件处理程序,请及时删除...",
     activleAuthor: "洛琪希",
-    activleUserPhoto: "img/user-images/wwy.jpg",
+    activleUserPhoto: "img/user-images/wwy.webp",
   },
   {
     activleId: "JavaScriptDomApi整理",
@@ -49,7 +49,7 @@ window._activleArray = [
     activleTag: "JavaScript",
     activleBody: `NodeList 对象独特的地方在于，它其实是一个对 DOM 结 构的查询，因此 DOM 结构的变化会自动地在 NodeList 中反映出来。我们通常说 NodeList 是实时的 活动对象，而不是第一次访问时所获得内容的快照。使用 Array.prototype.  slice()可以把 NodeList 对象转换为数组`,
     activleAuthor: "洛琪希",
-    activleUserPhoto: "img/user-images/wwy.jpg",
+    activleUserPhoto: "img/user-images/wwy.webp",
   },
   {
     activleId: "JavaScript常用封装方法",
@@ -62,7 +62,7 @@ window._activleArray = [
     activleTag: "JavaScript",
     activleBody: `计算文件大小封装方法、通用 requestAnimationFrame 封装方法、 实现非阻塞的暂停运行、返回搜索匹配中的字符串,并在前后加上相应的标签、返回匹配字符串在目标字符串中的起止位置下标、检查浏览器中存在的插件`,
     activleAuthor: "洛琪希",
-    activleUserPhoto: "img/user-images/wwy.jpg",
+    activleUserPhoto: "img/user-images/wwy.webp",
   },
   {
     activleId: "JavaScript原型链",
@@ -75,20 +75,7 @@ window._activleArray = [
     activleTag: "JavaScript",
     activleBody: `JavaScript的对象通过其身上原型这一点特征，来实现与传统的面向对象编程语言截然不同的继承机制。在JS中大多数情况下创建的对象，都拥有一个原型对象，创建的对象，从其原型为模板、来继承方法和属性。而原型对象本身都有可能拥有原型，并从中获取方法和属性，以此类推，这种关系便被称为原型链，原型链的关系解释了为何一个对象身上会拥有本不属于他的属性和方法`,
     activleAuthor: "洛琪希",
-    activleUserPhoto: "img/user-images/wwy.jpg",
-  },
-  {
-    activleId: 6,
-    isHot: true,
-    tags: [6],
-    activlePhoto: "img/activle-images/6.jpg",
-    activleTitle: "尤雨溪 在 2019 JSConf 的分享 Seeking the          6",
-    activleTime: new Date(2022, 6, 26).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `服务范围是个比较难翻译的单词，在原 PPT 中用了 “Scope” 这个单词表示，可以理解为 “作用域、框架的承诺功能范围、服务配套齐全程度”。比如提供的是一个工具库还是整体框架，插件管理是集中式还是依赖生态。
-      React 是典型的小服务范围框架，核心包只实现了基本功能，而其他生态基本靠社区拓展；Angular 是典型大服务范围框架，官方对所有业务场景都做了最佳实践能力覆盖；Vue 处在中间区域，通过功能分层，既拥有小服务范围的能力，又可以搭配官方插件实现更多场景化能力`,
-    activleAuthor: "洛琪希",
-    activleUserPhoto: "img/user-images/p.jpg",
+    activleUserPhoto: "img/user-images/wwy.webp",
   },
   {
     activleId: "MySQL基本命令整理",
@@ -101,183 +88,7 @@ window._activleArray = [
     activleTag: "JavaScript",
     activleBody: `MySQL基本命令整理`,
     activleAuthor: "洛琪希",
-    activleUserPhoto: "img/user-images/wwy.jpg",
-  },
-  {
-    activleId: 2,
-    tags: [8],
-    activlePhoto: "img/activle-images/2.png",
-    activleTitle: "JavaScript is a programming language   8",
-    activleTime: new Date(2022, 6, 24).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `JavaScript is a programming language that adds interactivity to your website. This happens in games, in the behavior of responses when buttons are pressed or with data entry on forms; with dynamic styling; with animation, etc. This article helps you get started with JavaScript and furthers your understanding of what is possible`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 3,
-    tags: [5],
-    activlePhoto: "img/activle-images/3.jpg",
-    activleTitle: "日期选择器   9",
-    activleTime: new Date(2022, 6, 23).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `日期选择器作为基础组件重要不可或缺的一员，大家已经快习惯它一成不变的样子，输入框+日期选择弹出层。但到业务中，这种墨守成规的样子真的能百分百契合业务需求吗。这篇文章从多个网站的日期选择场景出发，企图归纳出日期选择器的最佳实践。这篇文章对移动端的日期选择暂无涉猎，都是 PC 端，列举出通用场景，每个类型日期选择器需要考虑的设计。 文章链接：Designing The Perfect Date And Time Picker 感谢本期评论官 @黄子毅 @流形 @王亮 @赵阳 @不知名的花瓣工程师`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 4,
-    isHot: true,
-    tags: [4],
-    activlePhoto: "img/activle-images/4.jpg",
-    activleTitle:
-      "国内目前处于 BI 1.0 阶段，也就是报表阶段，因此笔者将阐述这个阶段 BI 的核心开发概念    10",
-    activleTime: new Date(2022, 6, 22).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `数据集即数据的集合，在 BI 领域更多指一种标准化的数据结构。
-      任何数据都可以封装成数据集，比如 txt 文本、excel、mysql 数据库等等。
-      数据集的基本形态是二维表格，列头表示字段，每一行就是一份数据，数据展示就是通过对这些数据字段进行多维度分析`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 5,
-    isHot: true,
-    activlePhoto: "img/activle-images/5.jpg",
-    activleTitle:
-      "javascript 的 this 是个头痛的话题，本期精读的文章更是引出了一个观点，避免使用 this。我们来看看是否有道理   11",
-    activleTime: new Date(2022, 6, 22).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `初学者可能突然将 this 弄丢导致程序出错，甚至在 react 中也要使用 bind 的方式，使回调可以访问到 setState 等函数。
-      this 也不利于测试，如果使用纯函数，可以通过入参出参做测试，而不需要预先初始化环境。`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 6,
-    isHot: true,
-    activlePhoto: "img/activle-images/6.jpg",
-    activleTitle:
-      "尤雨溪 在 2019 JSConf 的分享 Seeking the Balance in Framework Design 十分精彩，道出了如何进行合理的前端框架设计与框架选型    12",
-    activleTime: new Date(2022, 6, 22).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `服务范围是个比较难翻译的单词，在原 PPT 中用了 “Scope” 这个单词表示，可以理解为 “作用域、框架的承诺功能范围、服务配套齐全程度”。比如提供的是一个工具库还是整体框架，插件管理是集中式还是依赖生态。
-      React 是典型的小服务范围框架，核心包只实现了基本功能，而其他生态基本靠社区拓展；Angular 是典型大服务范围框架，官方对所有业务场景都做了最佳实践能力覆盖；Vue 处在中间区域，通过功能分层，既拥有小服务范围的能力，又可以搭配官方插件实现更多场景化能力`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 1,
-    activlePhoto: "img/activle-images/1.jpg",
-    activleTitle: "Title Goes Hear        13",
-    activleTime: new Date(2022, 6, 27).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
-      explicabo quis illo possimus aspernatur iusto, praesentium dolores,`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 2,
-    isHot: true,
-    activlePhoto: "img/activle-images/2.png",
-    activleTitle: "JavaScript is a programming language",
-    activleTime: "2022年7月12日13:54:00                  14",
-    activleTag: "JavaScript",
-    activleBody: `JavaScript is a programming language that adds interactivity to your website. This happens in games, in the behavior of responses when buttons are pressed or with data entry on forms; with dynamic styling; with animation, etc. This article helps you get started with JavaScript and furthers your understanding of what is possible`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 3,
-    isHot: true,
-    activlePhoto: "img/activle-images/3.jpg",
-    activleTitle: "日期选择器                 15",
-    activleTime: new Date(2022, 6, 27).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `日期选择器作为基础组件重要不可或缺的一员，大家已经快习惯它一成不变的样子，输入框+日期选择弹出层。但到业务中，这种墨守成规的样子真的能百分百契合业务需求吗。这篇文章从多个网站的日期选择场景出发，企图归纳出日期选择器的最佳实践。这篇文章对移动端的日期选择暂无涉猎，都是 PC 端，列举出通用场景，每个类型日期选择器需要考虑的设计。 文章链接：Designing The Perfect Date And Time Picker 感谢本期评论官 @黄子毅 @流形 @王亮 @赵阳 @不知名的花瓣工程师`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 4,
-    activlePhoto: "img/activle-images/4.jpg",
-    activleTitle:
-      "国内目前处于 BI 1.0 阶段，也就是报表阶段，因此笔者将阐述这个阶段 BI 的核心开发概念              16",
-    activleTime: new Date(2022, 6, 27).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `数据集即数据的集合，在 BI 领域更多指一种标准化的数据结构。
-      任何数据都可以封装成数据集，比如 txt 文本、excel、mysql 数据库等等。
-      数据集的基本形态是二维表格，列头表示字段，每一行就是一份数据，数据展示就是通过对这些数据字段进行多维度分析`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 5,
-    activlePhoto: "img/activle-images/5.jpg",
-    activleTitle:
-      "javascript 的 this 是个头痛的话题，本期精读的文章更是引出了一个观点，避免使用 this。我们来看看是否有道理             17",
-    activleTime: new Date(2022, 6, 27).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `初学者可能突然将 this 弄丢导致程序出错，甚至在 react 中也要使用 bind 的方式，使回调可以访问到 setState 等函数。
-      this 也不利于测试，如果使用纯函数，可以通过入参出参做测试，而不需要预先初始化环境。`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 6,
-    activlePhoto: "img/activle-images/6.jpg",
-    activleTitle:
-      "尤雨溪 在 2019 JSConf 的分享 Seeking the Balance in Framework Design 十分精彩，道出了如何进行合理的前端框架设计与框架选型             18",
-    activleTime: new Date(2022, 6, 27).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `服务范围是个比较难翻译的单词，在原 PPT 中用了 “Scope” 这个单词表示，可以理解为 “作用域、框架的承诺功能范围、服务配套齐全程度”。比如提供的是一个工具库还是整体框架，插件管理是集中式还是依赖生态。
-      React 是典型的小服务范围框架，核心包只实现了基本功能，而其他生态基本靠社区拓展；Angular 是典型大服务范围框架，官方对所有业务场景都做了最佳实践能力覆盖；Vue 处在中间区域，通过功能分层，既拥有小服务范围的能力，又可以搭配官方插件实现更多场景化能力`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 1,
-    activlePhoto: "img/activle-images/1.jpg",
-    activleTitle: "Title Goes Hear                  19",
-    activleTime: new Date(2022, 6, 27).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis
-      explicabo quis illo possimus aspernatur iusto, praesentium dolores,`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 2,
-    activlePhoto: "img/activle-images/2.png",
-    activleTitle: "JavaScript is a programming language               20",
-    activleTime: new Date(2022, 6, 27).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `JavaScript is a programming language that adds interactivity to your website. This happens in games, in the behavior of responses when buttons are pressed or with data entry on forms; with dynamic styling; with animation, etc. This article helps you get started with JavaScript and furthers your understanding of what is possible`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-
-  {
-    activleId: 4,
-    activlePhoto: "img/activle-images/4.jpg",
-    activleTitle:
-      "国内目前处于 BI 1.0 阶段，也就是报表阶段，因此笔者将阐述这个阶段 BI 的核心开发概念                      22",
-    activleTime: new Date(2022, 6, 27).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `数据集即数据的集合，在 BI 领域更多指一种标准化的数据结构。
-      任何数据都可以封装成数据集，比如 txt 文本、excel、mysql 数据库等等。
-      数据集的基本形态是二维表格，列头表示字段，每一行就是一份数据，数据展示就是通过对这些数据字段进行多维度分析`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 5,
-    activlePhoto: "img/activle-images/5.jpg",
-    activleTitle:
-      "javascript 的 this 是个头痛的话题，本期精读的文章更是引出了一个观点，避免使用 this。我们来看看是否有道理                        23",
-    activleTime: new Date(2022, 6, 27).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `初学者可能突然将 this 弄丢导致程序出错，甚至在 react 中也要使用 bind 的方式，使回调可以访问到 setState 等函数。
-      this 也不利于测试，如果使用纯函数，可以通过入参出参做测试，而不需要预先初始化环境。`,
-    activleUserPhoto: "img/user-images/p.jpg",
-  },
-  {
-    activleId: 6,
-    activlePhoto: "img/activle-images/6.jpg",
-    activleTitle:
-      "尤雨溪 在 2019 JSConf 的分享 Seeking the Balance in Framework Design 十分精彩，道出了如何进行合理的前端框架设计与框架选型            24",
-    activleTime: new Date(2022, 6, 27).getTime(),
-    activleTag: "JavaScript",
-    activleBody: `服务范围是个比较难翻译的单词，在原 PPT 中用了 “Scope” 这个单词表示，可以理解为 “作用域、框架的承诺功能范围、服务配套齐全程度”。比如提供的是一个工具库还是整体框架，插件管理是集中式还是依赖生态。
-      React 是典型的小服务范围框架，核心包只实现了基本功能，而其他生态基本靠社区拓展；Angular 是典型大服务范围框架，官方对所有业务场景都做了最佳实践能力覆盖；Vue 处在中间区域，通过功能分层，既拥有小服务范围的能力，又可以搭配官方插件实现更多场景化能力`,
-    activleUserPhoto: "img/user-images/p.jpg",
+    activleUserPhoto: "img/user-images/wwy.webp",
   },
 ];
 
